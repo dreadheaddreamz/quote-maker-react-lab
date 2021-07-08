297df11dbb345f71c8e67bded9759463e9a61a4d
dreadheaddreamz <github email address>
2021-07-07 21:31:41 -0500

    commit tree fa5f4b7e9439bf0db2d6e6b372c712c8c964fdd2

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,25 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    
+
+      default:
+        return state;
+  }
 }
+  
+  
+  
+  
+  
+  
